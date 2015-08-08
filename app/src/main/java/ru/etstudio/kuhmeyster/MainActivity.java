74ffd436d2380a1fef35c190bc118b8081ad49cb
teoretik <pianizd@gmail.com>
2015-08-08 20:38:33 +0300

    проба пера

--- app/src/main/java/ru/etstudio/kuhmeyster/MainActivity.java
+++ app/src/main/java/ru/etstudio/kuhmeyster/MainActivity.java
@@ -2,15 +2,21 @@ package ru.etstudio.kuhmeyster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
